36b1e3016ed56f25508baf9a1a030ed8e399fe0b
Ethel C <cofresi.ethel@gmail.com>
2018-10-12 14:37:13 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,19 +1,6 @@
-class SessionsController < ApplicationController
+class SecretsController < ApplicationController
 
-  def new
-  end
-
-  def create
-    if params[:name] == nil || params[:name].strip == ""
-      redirect_to '/login'
-    else
-      session[:name] = params[:name]
-      redirect_to '/'
-    end
-  end
-
-  def destroy
-    session.delete :name
-    redirect_to '/login'
+  def show
+      redirect_to '/login' if !current_user
   end
 end
