058f01dbda206bdf1332eac3acc7190ab451667d
Ethel C <cofresi.ethel@gmail.com>
2018-10-12 14:32:03 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,11 +1,6 @@
 class SecretsController < ApplicationController
-    before_action :require_login
-    
-    def show
-    end
-
-    private 
-    def require_login
-        redirect_to controller: 'sessions', action: 'new' unless current_user
-      end
+  before_action :require_logged_in
+  
+  def show
+  end
 end
