8a1c2837e448c24d72a485b4925897d0c09d3297
Ethel C <cofresi.ethel@gmail.com>
2018-10-12 14:37:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
-    def new
-    end
 
-    def create
-        if params[:name].nil? || params[:name].empty?
-            redirect_to login_path
-        else
-            session[:name] = params[:name]
-            redirect_to root_path
-        end
-    end
+  def new
+  end
 
-    def destroy
-        session.delete :name
-        redirect_to login_path
+  def create
+    if params[:name] == nil || params[:name].strip == ""
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
     end
+  end
 
+  def destroy
+    session.delete :name
+    redirect_to '/login'
+  end
 end
